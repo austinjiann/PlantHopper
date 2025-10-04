7c68e7b084ef5d63901aec3a52b99abb6b27e852
pekachoo <jliu6162@gmail.com>
2025-10-04 08:48:47 -0400

    command implementation

--- Arduino_Files/UniversalControl/UniversalControl.ino
+++ Arduino_Files/UniversalControl/UniversalControl.ino
@@ -42,70 +42,87 @@ void loop() {
   line.trim();   // remove CR/LF or spaces
 
   int idx_cmd   = line.indexOf("cmd:");
-  int idx_id    = line.indexOf("id:");
-  int idx_found = line.indexOf("found:");
-  int idx_dx    = line.indexOf("dx:");
-  int idx_pitch = line.indexOf("pitch:");
-  int idx_shoot = line.indexOf("shoot:");
-
   if (idx_cmd >= 0) {
     String s = line.substring(idx_cmd + 4, line.indexOf(';', idx_cmd));
     cmd = s;   // String type
   }
 
-  if (idx_id >= 0) {
-    String s = line.substring(idx_id + 3, line.indexOf(';', idx_id));
-    cmdId = s.toInt();   // integer
-  }
+  if(cmd == "SEARCH"){
 
-  if (idx_found >= 0) {
-    String s = line.substring(idx_found + 6, line.indexOf(';', idx_found));
-    s.toLowerCase();
-    cmdFound = (s == "true");  // boolean
-  }
+    int idx_id    = line.indexOf("id:");
+    int idx_found = line.indexOf("found:");
+    int idx_dx    = line.indexOf("dx:");
+    int idx_pitch = line.indexOf("pitch:");
+    int idx_shoot = line.indexOf("shoot:");
 
-  if (idx_dx >= 0) {
-    String s = line.substring(idx_dx + 3, line.indexOf(';', idx_dx));
-    cmdDx = s.toFloat();   // double / float
-  }
-
-  if (idx_pitch >= 0) {
-    String s = line.substring(idx_pitch + 6, line.indexOf(';', idx_pitch));
-    cmdPitch = s.toInt();   // int
-  }
-
-  if (idx_shoot >= 0) {
-    String s = line.substring(idx_shoot + 6, line.indexOf(';', idx_shoot));
-    s.toLowerCase();
-    cmdShoot = (s == "true");  // boolean
-  }
-
-  if(idx_found){
-    //run PID to align
-    Serial.println("index found");
-    currTurretPos += cmdDx*kP;
-    if(currTurretPos > 300){
-      currTurretPos = 300;
+    if (idx_id >= 0) {
+      String s = line.substring(idx_id + 3, line.indexOf(';', idx_id));
+      cmdId = s.toInt();   // integer
     }
-    if(currTurretPos < 0){
-      currTurretPos = 0;
+
+    if (idx_found >= 0) {
+      String s = line.substring(idx_found + 6, line.indexOf(';', idx_found));
+      s.toLowerCase();
+      cmdFound = (s == "true");  // boolean
     }
-    turret.write(convertTurretAngle(currTurretPos));
+
+    if (idx_dx >= 0) {
+      String s = line.substring(idx_dx + 3, line.indexOf(';', idx_dx));
+      cmdDx = s.toFloat();   // double / float
+    }
+
+    if (idx_pitch >= 0) {
+      String s = line.substring(idx_pitch + 6, line.indexOf(';', idx_pitch));
+      cmdPitch = s.toInt();   // int
+    }
+
+    if (idx_shoot >= 0) {
+      String s = line.substring(idx_shoot + 6, line.indexOf(';', idx_shoot));
+      s.toLowerCase();
+      cmdShoot = (s == "true");  // boolean
+    }
+
+    if(idx_found){
+      //run PID to align
+      Serial.println("index found");
+      currTurretPos += cmdDx*kP;
+      if(currTurretPos > 300){
+        currTurretPos = 300;
+      }
+      if(currTurretPos < 0){
+        currTurretPos = 0;
+      }
+      turret.write(convertTurretAngle(currTurretPos));
+      pitch.write(convertPitchAngle(cmdPitch));
+    }
+  }
+  else if(cmd == "SHOOT"){
+    //format 
+    int pitch_id = line.indexOf("pitch:");
+    if (pitch_id >= 0) {
+      String s = line.substring(pitch_id + 3, line.indexOf(';', pitch_id));
+      cmdPitch = s.toInt(); 
+    }
+
+    int idx_dx = line.indexOf("dx:");
+    if (idx_dx >= 0) {
+      String s = line.substring(idx_dx + 3, line.indexOf(';', idx_dx));
+      cmdDx = s.toFloat();   // double / float
+    }
+    //alignment code
+    if(cmdDx < 0.02){
+      //SHOOT
+    }
+    else{
+      currTurretPos += cmdDx*kP;
+      if(currTurretPos > 300){
+        currTurretPos = 300;
+      }
+      if(currTurretPos < 0){
+        currTurretPos = 0;
+      }
+      turret.write(convertTurretAngle(currTurretPos));
+    }
     pitch.write(convertPitchAngle(cmdPitch));
   }
-
-  // myServo2.write(140);
-
-
-  // myServo.write(0);
-
-  // myServo.write(165);
-  // myServo2.write(165);
-
-
-  // delay(2000);
-
-  // myServo.write(100);
-
-  // delay(2000);
 }
